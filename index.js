5f9ed244bcc034d6547d356b364812c08afd0ef1
cipater <cipater@users.noreply.github.com>
2020-02-25 11:08:36 -0800

    Generate documentId that is consistent with apollo client:extract

--- index.js
+++ index.js
@@ -1,109 +1,169 @@
-const vm = require('vm');
-const os = require('os');
-const loaderUtils = require('loader-utils');
-const { ExtractGQL } = require('persistgraphql/lib/src/ExtractGQL');
-const queryTransformers = require('persistgraphql/lib/src/queryTransformers');
+const vm = require('vm')
+const os = require('os')
+const loaderUtils = require('loader-utils')
+const { GraphQLError, visit, Kind, separateOperations } = require('graphql')
+const { operationHash, operationRegistrySignature } = require('apollo-graphql')
 
-module.exports = function graphQLPersistedDocumentLoader(content) {
-  const deps = [];
-  const context = this;
-  const options = loaderUtils.getOptions(this) || {};
+module.exports = function graphQLPersistedDocumentLoader(
+  graphQLTagLoaderSource
+) {
+  const context = this
+  const options = loaderUtils.getOptions(this) || {}
+  const { document, dependencies } = getDocumentAndDependenciesFromSource(
+    graphQLTagLoaderSource,
+    context
+  )
 
-  // Create a fake sandbox to intercept the query dependencies when
-  // executing the generated code from the graphql-tag loader.
+  this._module._graphQLDocuments = [document]
+
+  const callback = this.async()
+
+  // Wait for dependencies (if any) to resolve so we can merge the aggregated
+  // ... documents and hash the "printed" results of the AST
+  Promise.all(dependencies)
+    .then(modules => {
+      // collect the documents from dependencies
+      modules.map(mod => {
+        this._module._graphQLDocuments = this._module._graphQLDocuments.concat(
+          mod.module._graphQLDocuments
+        )
+      })
+
+      const operations = mergedOperationsAndFragments(
+        this._module._graphQLDocuments
+      )
+
+      for (let [operationName, operationAST] of Object.entries(operations)) {
+        if (options.addTypename)
+          operationAST = withTypenameFieldAddedWhereNeeded(operationAST)
+
+        const printed = operationRegistrySignature(
+          operationAST,
+          operationName,
+          {
+            preserveStringAndNumericLiterals:
+              options.preserveStringAndNumericLiterals
+          }
+        )
+
+        const id = JSON.stringify(operationHash(printed))
+
+        graphQLTagLoaderSource += `${os.EOL}module.exports["${operationName}"].documentId = ${id};`
+      }
+
+      callback(null, graphQLTagLoaderSource)
+    })
+    .catch(err => {
+      console.log('error', err)
+      callback(err)
+    })
+}
+
+function getDocumentAndDependenciesFromSource(source, context) {
+  const dependencies = []
   const sandbox = {
     require(file) {
-      deps.push(new Promise((resolve, reject) => {
-        context.loadModule(file, (err, source, sourceMap, module) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve({ source, sourceMap, module });
-          }
-        });
-      }));
-      return { definitions: [] };
+      dependencies.push(
+        new Promise((resolve, reject) => {
+          context.loadModule(file, (err, source, sourceMap, module) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve({ source, sourceMap, module })
+            }
+          })
+        })
+      )
+      return { definitions: [] }
     },
     module: {
       exports: null
     }
-  };
+  }
+
   // Run the graphql-tag loader generated code to capture the dependencies.
-  vm.runInNewContext(content, sandbox);
+  vm.runInNewContext(source, sandbox)
 
-  // Get the query document source from the exported module, and
-  // save it so that we can use it from other modules that may depend on
-  // this one.
-  const doc = sandbox.module.exports;
-  this._module._graphQLQuerySource = doc.loc.source.body;
-
-  if (deps.length === 0) {
-    // If no deps, just try to generate the document id from the
-    // source returned from graphql-tag loader result. This will
-    // add an id only if the source contains an operation.
-    content = tryAddDocumentId(options, content, this._module._graphQLQuerySource);
-    return content;
-  }
-
-  const callback = this.async();
-
-  // If we have dependencies, we retrieve their query sources and
-  // concatenate them with the one from this module, to create the
-  // full query source.
-  Promise.all(deps).then((modules) => {
-    modules.forEach((mod, index) => {
-      this._module._graphQLQuerySource += mod.module._graphQLQuerySource;
-    });
-
-    try {
-      // Now that we have all our dependencies' sources concatenated
-      // with this module's query source, we can send all that to
-      // generate the document id, if the resulting source
-      // is for an operation.
-      content = tryAddDocumentId(options, content, this._module._graphQLQuerySource);
-    } catch (e) {
-      callback(e);
-    }
-
-    callback(null, content);
-  }).catch((err) => {
-    console.log('error', err);
-    callback(err);
-  });
-};
-
-function tryAddDocumentId(options, content, querySource) {
-  const queryMap = new ExtractGQL({
-    queryTransformers: [options.addTypename && queryTransformers.addTypenameTransformer].filter(Boolean)
-  }).createOutputMapFromString(querySource);
-
-  const queries = Object.keys(queryMap);
-  if (queries.length > 1) {
-    queries
-      .map(query => {
-        const matched = query.match(/^(mutation|query)\ ([^\ \(\{]*)/)
-        if (!matched) {
-          return false
-        }
-        return {
-          operationName: matched[2],
-          id: generateIdForQuery(options, query)
-        }
-      })
-      .filter(isValid => !!isValid)
-      .forEach(({ id, operationName }) => {
-        content += `${os.EOL}module.exports["${operationName}"].documentId = ${JSON.stringify(id)};`
-      })
-  } else if (queries.length === 1) {
-    const queryId = generateIdForQuery(options, Object.keys(queryMap)[0]);
-    content += `${os.EOL}doc.documentId = ${JSON.stringify(queryId)}`;
-  }
-
-  return content;
+  return { document: sandbox.module.exports, dependencies }
 }
 
-function generateIdForQuery(options, query) {
-  if (options.generateId) return options.generateId(query);
+function mergedOperationsAndFragments(documents) {
+  return separateOperations({
+    kind: Kind.DOCUMENT,
+    definitions: [
+      ...Object.values(fragments(documents)),
+      ...Object.values(operations(documents))
+    ]
+  })
+}
 
-  return require('crypto').createHash('sha256').update(query).digest('hex');
+function fragments(documents) {
+  const fragments = Object.create(null)
+
+  for (const document of documents) {
+    for (const definition of document.definitions) {
+      if (definition.kind === Kind.FRAGMENT_DEFINITION) {
+        fragments[definition.name.value] = definition
+      }
+    }
+  }
+
+  return fragments
+}
+
+function operations(documents) {
+  const operations = Object.create(null)
+
+  for (const document of documents) {
+    for (const definition of document.definitions) {
+      if (definition.kind === Kind.OPERATION_DEFINITION) {
+        if (!definition.name) {
+          throw new GraphQLError(
+            'Apollo does not support anonymous operations',
+            [definition]
+          )
+        }
+        operations[definition.name.value] = definition
+      }
+    }
+  }
+
+  return operations
+}
+
+const typenameField = {
+  kind: Kind.FIELD,
+  name: { kind: Kind.NAME, value: '__typename' }
+}
+
+function withTypenameFieldAddedWhereNeeded(ast) {
+  return visit(ast, {
+    enter: {
+      SelectionSet(node) {
+        return {
+          ...node,
+          selections: node.selections.filter(
+            selection =>
+              !(
+                selection.kind === 'Field' &&
+                selection.name.value === '__typename'
+              )
+          )
+        }
+      }
+    },
+    leave(node) {
+      if (!(node.kind === 'Field' || node.kind === 'FragmentDefinition'))
+        return undefined
+      if (!node.selectionSet) return undefined
+
+      return {
+        ...node,
+        selectionSet: {
+          ...node.selectionSet,
+          selections: [typenameField, ...node.selectionSet.selections]
+        }
+      }
+    }
+  })
 }
